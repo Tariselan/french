f4b07248891d36904a86922fb8a3de63a6222fd7
Tariselan <woodmoreaaron@gmail.com>
2023-02-03 13:18:41 +1300

    Laid some basic groundwork

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
-
+// consts and vars
+const userInput = document.getElementById('textbox');
+// const words = [page1, page2];
+const page1 = {
+    french: [
+        "à peu près",
+        "à propos de",
+        "à travers",
+        "abondonner",
+        "l'abus de", // m
+        "accompagner",
+    ],
+    english: [
+        "approximately",
+        "in regards to",
+        "through",
+        "to abandon",
+        "the abuse of",
+        "to accompany",
+    ]
+}
